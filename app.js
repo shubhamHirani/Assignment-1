b4e0c027b56e74f5727183943cbdf211249ff7f7
shubhamHirani <97084275+shubhamHirani@users.noreply.github.com>
2022-01-11 15:30:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//fetchig the required modules
 const { response } = require('express')
 const res = require('express/lib/response')
 const request = require('request')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+//create csvWriter object to define csv file headers
 const csvWriter = createCsvWriter({
     path: './data.csv',
     header: [
@@ -16,18 +18,22 @@ const csvWriter = createCsvWriter({
 });
  
 
-
+//fetch object to define URL and header types and json data
 const fetch= {url: 'https://api.github.com/search/repositories?q=is:public',
  headers: {
     "User-Agent" : "request"
   }, json: true
 }
+
+//callback function takes error and response as input
 function callback(error, response)
 {
   if(!error){
   
+      //array to store the data
   let getData = [] ;
-    
+      
+    //for loop to traverse through dataset
   response.body.items.forEach(item => {
     if( item.forks_count >= 100 & item.language == 'JavaScript'){
     console.log("---------------------------------------------------------")
@@ -37,6 +43,8 @@ function callback(error, response)
     console.log('Watchers count: ' +item.watchers_count)
     console.log("Stargazers count: " +item.stargazers_count)
     console.log("Forks Count: " +item.forks_count)
+        
+        //push data inside an array
     if(item.stargazers_count>200){
       dataArray = {name: item.name, description: item.description,
          html_url : item.html_url, watchers_count: item.watchers_count, 
@@ -46,7 +54,7 @@ function callback(error, response)
     }
   }
   });
-
+//writing into csv file
    if(data.length > 0){
     csvWriter.writeRecords(data)       
     .then(() => {
@@ -60,6 +68,7 @@ function callback(error, response)
 }
 }
 
+//function caling
 request(fetch, callback)
 
 
